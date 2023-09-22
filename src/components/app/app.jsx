9e3c46155d7644675ce9d5e0820e52820d891cbf
Wladisalw Fedoseev <wladislawfedoseev@gmail.com>
2023-09-22 11:36:11 +0300

    add components

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,26 @@
-import AppBanner from "../appBanner/appBanner"
+import AppBanner from "../appBanner/appBanner";
+import AppHeader from "../appHeader/appHeader";
+import RandomChar from "../randomChar/randomChar";
+import CharList from "../charList/charList";
+import CharInfo from "../charInfo/charInfo";
+import ComicsList from "../comicsList/comicsList";
+
+
+import decoration from '../../resources/img/vision.png';
 
 const App = () => {
     return (
         <div className="app">
             <AppBanner />
+            <AppHeader />
+            <main>
+                <RandomChar />
+                <div className="char__content">
+                    <CharList />
+                    <CharInfo />
+                </div>
+                <img className="bg-decoration" src={decoration} alt="vision" />
+            </main>
         </div>
     )
 }
