8dd9c24176c11b373b1631b88b2a4e70396db46d
Wladisalw Fedoseev <wladislawfedoseev@gmail.com>
2023-10-02 11:39:57 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import './style/style.scss';
  
+
+ 
+ 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
