fbf16601e47ee8695a76a71e1770f6aaab868d85
Wladisalw Fedoseev <wladislawfedoseev@gmail.com>
2023-10-02 11:38:24 +0300

    add pages in main index

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import MainPage from "./MainPage";
+import CosmicPage from "./CosmicPage";
+import Page404 from "./404";
+import SingleComicPage from "./singleComicPage/SingleComicPage";
+
+export {
+    MainPage,
+    CosmicPage,
+    Page404,
+    SingleComicPage
+}
